6a8b0c5a33df10f04369b8f9c09a77c3addc00a4
Simon <sajmon2325@gmail.com>
2025-03-16 20:04:50 +0100

    Added OpenAPI description

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "Expense Manager API",
+        Version = "v1",
+        Description = "A personal finance management system that allows users to track their transactions, " +
+                      "categorize expenses, assign beneficiaries, and associate purchases with stores. " +
+                      "It supports multiple currencies, role-based permissions, and detailed reporting " +
+                      "for better financial insights."
+    });
+});
 
 var app = builder.Build();
 
@@ -11,8 +24,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Expense Manager API v1"));}
 
 app.UseHttpsRedirection();
 
